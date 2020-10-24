a625c2107f376d56176910edcce39e8792438e17
Jose Vázquez Viader <jose.vazquez.viader@gmail.com>
2020-10-24 19:39:37 +0200

    Added option to send custom options to mirror

--- upload.sh
+++ upload.sh
@@ -58,5 +58,5 @@ lftp -e "set xfer:log 1; \
   set ssl:check-hostname $PLUGIN_VERIFY; \
   set net:max-retries 3; \
   $PLUGIN_CLEAN_DIR; \
-  mirror --verbose $PLUGIN_CHMOD -R $PLUGIN_INCLUDE_STR $PLUGIN_EXCLUDE_STR $(pwd)$PLUGIN_SRC_DIR $PLUGIN_DEST_DIR" \
+  mirror --verbose $PLUGIN_CHMOD -R $PLUGIN_MIRROR_OPTS $PLUGIN_INCLUDE_STR $PLUGIN_EXCLUDE_STR $(pwd)$PLUGIN_SRC_DIR $PLUGIN_DEST_DIR" \
   -u $FTP_USERNAME,$FTP_PASSWORD $PLUGIN_HOSTNAME
